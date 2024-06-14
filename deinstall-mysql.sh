0ccf52f2e517fb7d76b565f6fda92203341ccce8
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-14 19:01:38 +0200

    Update deinstall-mysql.sh

--- deinstall-mysql.sh
+++ deinstall-mysql.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-kubectl delete secret mysql-secret
-kubectl delete pv mysql-pv-volume
-kubectl delete pvc mysql-pv-claim
-kubectl delete deployment mysql
-kubectl delete service mysql-service
-kubectl delete deployment phpmyadmin-deployment
-kubectl delete service phpmyadmin-service
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"
+  do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
+execute() {
+  echo "Executing: $@"
+  if ! "$@"
+  then
+    echo "$(printf "Failed during: %s" "$(shell_join "$@")")"
+    exit 1
+  fi
+}
+
+execute "kubectl delete secret mysql-secret"
+execute "kubectl delete pv mysql-pv-volume"
+execute "kubectl delete pvc mysql-pv-claim"
+execute "kubectl delete deployment mysql"
+execute "kubectl delete service mysql-service"
+execute "kubectl delete deployment phpmyadmin-deployment"
+execute "kubectl delete service phpmyadmin-service"
