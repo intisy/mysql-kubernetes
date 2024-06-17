007ee7d6ef819bff9851bc08ec652037788a14e3
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-17 14:41:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-password=$1
+root_password=$1
+user_password=$2
+using_nfs=false
 
 generate_secure_password() {
   if ! command -v openssl &> /dev/null; then
@@ -11,16 +13,23 @@ generate_secure_password() {
   password=$(openssl rand -base64 $length | tr -dc 'A-Za-z0-9')
 }
 
-if [ ! -n "$password" ]; then
+if [ ! -n "$root_password" ]; then
   generate_secure_password
+  root_password=$password
 fi
-echo "Root password: $password"
+
+if [ ! -n "$user_password" ]; then
+  generate_secure_password
+  user_password=$password
+fi
 curl -fsSL https://raw.githubusercontent.com/WildePizza/kubernetes-apps/HEAD/uninstall.sh | bash -s false
-kubectl create secret generic mysql-root-pass --from-literal=password=$password
+echo "Root password: $password"
+kubectl create secret generic mysql-root-pass --from-literal=password=$root_password
 generate_secure_password
 echo "User password: $password"
-kubectl create secret generic mysql-user-pass --from-literal=password=$password
-kubectl apply -f - <<OEF
+kubectl create secret generic mysql-user-pass --from-literal=password=$user_password
+if [ "$using_nfs" = true ]; then
+  kubectl apply -f - <<OEF
 apiVersion: v1
 kind: PersistentVolume
 metadata:
@@ -35,6 +44,21 @@ spec:
     path: /exports/documents
   persistentVolumeReclaimPolicy: Recycle
 OEF
+else
+  kubectl apply -f - <<OEF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: mysql-pv
+spec:
+  capacity:
+    storage: 20Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: "/var/lib/mysql"
+OEF
+fi
 kubectl apply -f https://raw.githubusercontent.com/WildePizza/kubernetes-apps/HEAD/mysql.yaml
 kubectl apply -f https://raw.githubusercontent.com/WildePizza/kubernetes-apps/HEAD/phpmyadmin.yaml
 while [ $(kubectl get deployment mysql | grep -c "1/1") != "1" ]; do
