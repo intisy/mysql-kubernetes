2dd45cb26df5a181c0dd4b2bbf3a8335d70ce9b0
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-18 16:31:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,10 +23,9 @@ if [ ! -n "$user_password" ]; then
   user_password=$password
 fi
 curl -fsSL https://raw.githubusercontent.com/WildePizza/kubernetes-apps/HEAD/uninstall.sh | bash -s false
-echo "Root password: $password"
+echo "Root password: $root_password"
 kubectl create secret generic mysql-root-pass --from-literal=password=$root_password
-generate_secure_password
-echo "User password: $password"
+echo "User password: $user_password"
 kubectl create secret generic mysql-user-pass --from-literal=password=$user_password
 if [ "$using_nfs" = true ]; then
   kubectl apply -f - <<OEF
