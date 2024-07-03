e63fbf704a1b7030e0e6291f3dcff759dc30cce7
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-03 15:45:26 +0200

    Update deinstall.sh

--- deinstall.sh
+++ deinstall.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
-log=$1
-
-shell_join() {
-  local arg
-  printf "%s" "$1"
-  shift
-  for arg in "$@"
-  do
-    printf " "
-    printf "%s" "${arg// /\ }"
-  done
-}
-
-execute() {
-  command="$@"
-  if [ "$log" = false ]; then
-    void=$($command >&2)
-  else
-    $command
-    if [[ $? -ne 0 ]]; then
-      echo "Error: '$command' failed with exit code: $?."
-    fi
-  fi
-}
-
-execute "kubectl delete secret mysql-root-pass --grace-period=0 --force"
-execute "kubectl delete secret mysql-user-pass --grace-period=0 --force"
-execute "kubectl delete deployment mysql --grace-period=0 --force"
-execute "kubectl delete service mysql --grace-period=0 --force"
-execute "kubectl delete deployment phpmyadmin --grace-period=0 --force"
-execute "kubectl delete service phpmyadmin --grace-period=0 --force"
-execute "kubectl delete pv mysql-pv --grace-period=0 --force"
-execute "kubectl delete pvc mysql-pv-claim --grace-period=0 --force"
+kubectl delete service phpmyadmin --grace-period=0 --force
+kubectl delete deployment phpmyadmin --grace-period=0 --force
+kubectl delete deployment mysql --grace-period=0 --force
+kubectl delete service mysql --grace-period=0 --force
+kubectl delete pvc mysql-pv-claim --grace-period=0 --force
+kubectl delete pv mysql-pv --grace-period=0 --force
+kubectl delete secret mysql-root-pass --grace-period=0 --force
+kubectl delete secret mysql-user-pass --grace-period=0 --force
