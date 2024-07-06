4b909b402bddba4f1cb1b186c97bcd194e4dd1ce
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-06 08:29:05 +0200

    Update deinstall.sh

--- scripts/deinstall.sh
+++ scripts/deinstall.sh
@@ -3,8 +3,10 @@
 kubectl delete service phpmyadmin --grace-period=0 --force
 kubectl delete deployment phpmyadmin --grace-period=0 --force
 kubectl delete pods -l app=phpmyadmin --grace-period=0 --force
+kubectl delete replicaset.apps -l app=phpmyadmin --grace-period=0 --force
 kubectl delete deployment mysql --grace-period=0 --force
 kubectl delete pods -l app=mysql --grace-period=0 --force
+kubectl delete replicaset.apps -l app=mysql --grace-period=0 --force
 kubectl delete service mysql --grace-period=0 --force
 kubectl delete pvc mysql-pv-claim --grace-period=0 --force
 kubectl delete pv mysql-pv --grace-period=0 --force
