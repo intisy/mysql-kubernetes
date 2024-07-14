7e53e7b417b9ef47511ade2aa033fb0ab75a5b96
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-14 14:46:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ execute() {
   substring="#!/bin/bash"
   sha=$(curl -sSL https://api.github.com/repos/WildePizza/mysql-kubernetes/commits?per_page=2 | jq -r '.[1].sha')
   url="https://raw.githubusercontent.com/WildePizza/mysql-kubernetes/HEAD/.commits/$sha/scripts/$action.sh"
-  echo "Executing: $url"
   output=$(curl -fsSL $url 2>&1)
   if [[ $output =~ $substring ]]; then
     curl -fsSL $url | bash -s $sha $arg1 $arg2
