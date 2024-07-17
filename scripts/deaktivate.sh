b86516c2091622ed1a0e815303786121f0a6ca4e
Pizza <wilderpizza99@gmail.com>
2024-07-17 14:37:40 +0200

    Delete deaktivate.sh

--- scripts/deaktivate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete deployment phpmyadmin
-kubectl delete pods -l app=phpmyadmin
-kubectl delete replicaset.apps -l app=phpmyadmin
-kubectl delete deployment mysql
-kubectl delete pods -l app=mysql
-kubectl delete replicaset.apps -l app=mysql
