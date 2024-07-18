1a618cea7d3afa8aaf7fc3f35001a124efd856f8
Pizza <wilderpizza99@gmail.com>
2024-07-18 13:33:48 +0200

    fixed runner

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 action=$1
-arg1=$2
-arg2=$2
+password=$2
+using_nfs=$3
 
 execute() {
   substring="#!/bin/bash"
-  sha=$(curl -sSL https://api.github.com/repos/WildePizza/mysql-kubernetes/commits?per_page=2 | jq -r '.[1].sha')
-  url="https://raw.githubusercontent.com/WildePizza/mysql-kubernetes/HEAD/.commits/$sha/scripts/$action.sh"
+  sha=$(curl -sSL https://api.github.com/repos/WildePizza/mysql-kubernetes/commits | jq -r '.[1].sha')
+  url="https://raw.githubusercontent.com/WildePizza/mysql-kubernetes/$sha/scripts/$action.sh"
   output=$(curl -fsSL $url 2>&1)
   if [[ $output =~ $substring ]]; then
-    curl -fsSL $url | bash -s $sha $arg1 $arg2
+    curl -fsSL $url | bash -s $sha $password $using_nfs
   else
     sleep 1
     execute
